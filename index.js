4a387f0868b128b984c0305b4b000667444c1124
xmerlinlor <xmerlinlordbot@gmail.com>
2026-09-03 11:23:03 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+{
+  "name": "queen-md",
+  "version": "1.0.0",
+  "description": "Queen MD WhatsApp Clan Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "engines": {
+    "node": ">=20"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.18",
+    "express": "^4.21.2",
+    "pino": "^9.6.0",
+    "qrcode": "^1.5.4"
+  }
+}
